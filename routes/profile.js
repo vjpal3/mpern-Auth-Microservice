5ce501501b10769ed5cf4412e416a9d8cc580d9e
vjpal3 <vrishalipal@yahoo.com>
2020-04-23 17:56:01 -0400

    Create user profile

--- routes/profile.js
+++ routes/profile.js
@@ -7,8 +7,16 @@ const auth = require('../middleware/auth');
 // @desc create profile
 // @access private
 
-router.post('/', auth, (req, res) => {
-  res.json(req.user);
+router.post('/', auth, async (req, res) => {
+  const authid = req.user.id;
+  const { first_name, last_name, avatar, github, cohort } = req.body;
+
+  let userProfile = await pool.query(
+    'Insert into profile(authid, first_name, last_name, avatar, github, cohort)     values ($1, $2, $3, $4, $5, $6) RETURNING *',
+    [authid, first_name, last_name, avatar, github, cohort]
+  );
+  userProfile = userProfile.rows[0];
+  res.json(userProfile);
 });
 
 //@route GET api/profiles/self
